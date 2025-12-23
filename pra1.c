0fd49ac1879faa1091eb42a98af39420a76d58ee
gpraveen29406-debug <gpraveen29406@gmail.com>
2025-12-23 22:43:38 +0530

    Add files via upload

---
+++ pra1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+# Input: Year
+year = int(input("Enter a year: "))
+
+# Check if the year is a leap year
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("Leap year")
+else:
+    print("Not a leap year")
+    
+    return 0;
+}
